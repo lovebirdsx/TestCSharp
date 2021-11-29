dafaffcb02001867587414bab0dbfa1e44e76fd2
lovebird <lovebirdsx@gmail.com>
2021-11-29 13:58:18 +0800

    up

--- project/conditional/Program.cs
+++ project/conditional/Program.cs
@@ -1,12 +1,10 @@
-﻿#define CONDITION1
+﻿// #define CONDITION1
 #define CONDITION2
 using System;
 using System.Diagnostics;
 
-class Test
-{
-    static void Main()
-    {
+class Test {
+    static void Main() {
         Console.WriteLine("Calling Method1");
         Method1(3);
         Console.WriteLine("Calling Method2");
@@ -16,20 +14,17 @@ class Test
     }
 
     [Conditional("CONDITION1")]
-    public static void Method1(int x)
-    {
+    public static void Method1(int x) {
         Console.WriteLine("CONDITION1 is defined");
     }
 
     [Conditional("CONDITION1"), Conditional("CONDITION2")]
-    public static void Method2()
-    {
+    public static void Method2() {
         Console.WriteLine("CONDITION1 or CONDITION2 is defined");
     }
 
     [Conditional("CONDITION2")]
-    public static void Method3()
-    {
+    public static void Method3() {
         Console.WriteLine("CONDITION2 is defined");
     }
 }
